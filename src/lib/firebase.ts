aace94827a0fe133c68fc2d1b7637b23d5e3915e
Kenya Mccullough <mcculloughkenya65@gmail.com>
2025-06-02 12:24:54 +0000

    // Import the functions you need from the SDKs you need import { initializeApp } from "firebase/app"; // TODO: Add SDKs for Firebase products that you want to use // https://firebase.google.com/docs/web/setup#available-libraries
    // Your web app's Firebase configuration
    const firebaseConfig = {
      apiKey: "AIzaSyD368AnFS8XL9FAgq59_7FXAVcg_DNoE9I",
      authDomain: "silzeycom-pos.firebaseapp.com",
      projectId: "silzeycom-pos",
      storageBucket: "silzeycom-pos.firebasestorage.app",
      messagingSenderId: "77954552274",
      appId: "1:77954552274:web:37e19d0f44c7ebf1f9380b"
    };
    
    // Initialize Firebase
    const app = initializeApp(firebaseConfig);

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,7 +9,8 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  // measurementId is not present in the new config, so it's removed or can be conditionally added if the env var exists
+  ...(process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID && { measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID }),
 };
 
 // Initialize Firebase
