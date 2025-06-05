ad27ae42862d0778c1e9298da4fd0906200e5af6
Kenya Mccullough <mcculloughkenya65@gmail.com>
2025-06-05 16:55:11 +0000

    Error: Invalid src prop (https://images.pexels.com/photos/7667726/pexels

--- next.config.js
+++
@@ -1,4 +0,0 @@
-// This file has been removed as its contents were consolidated into next.config.ts
-// to ensure a single source of truth for Next.js configuration.
-// The remotePatterns, including the one for 'lh3.googleusercontent.com',
-// have been merged into next.config.ts.
